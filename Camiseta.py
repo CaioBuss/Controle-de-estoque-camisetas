d69cbc1afbfdfe2e0c892270b4ff862a94d64247
CaioBuss <caiofdab@gmail.com>
2026-03-17 18:38:11 -0300

    Classes + index

---
+++ Camiseta.py
@@ -0,0 +1,6 @@
+class Camiseta:
+    def __init__(self, ID, nome, quantidade, valor):
+        self.ID = ID
+        self.nome = nome
+        self.quantidade = quantidade
+        self.valor = valor
---
+++ Venda.py
@@ -0,0 +1,6 @@
+class Venda:
+    def __init__ (self, cliente, camiseta, quantidade, valor_total):
+        self.cliente = cliente
+        self.camiseta = camiseta
+        self.quantidade = quantidade
+        self.valor_total = valor_total
