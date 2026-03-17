5ab35f63019e25cebd41762f33f7f583f363c7d1
CaioBuss <caiofdab@gmail.com>
2026-03-17 19:37:45 -0300

    Layout Menu

--- index.py
+++ index.py
@@ -6,7 +6,22 @@ camiseta = ListaEncadeada()
 venda = Fila()
 pilha = Pilha()
 
+def menu():
+    print('''
+===== MENU ESTOQUE =====
+1 - Cadastrar cliente
+2 - Listar clientes
+3 - Cadastrar produto
+4 - Listar produtos
+5 - Pesquisar produto
+6 - Realizar venda
+7 - Ver fila de vendas
+8 - Desfazer última operação
+9 - Exibir valor total do estoque
+10 - Exibir valor total de vendas
+11 - Exibir clientes e valores gastos
+12 - Sair
+========================
 
-
-
+          ''')
 
